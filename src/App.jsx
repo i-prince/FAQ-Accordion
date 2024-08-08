2e3a8ad202abe21ae2642022fee4d78d68f5483a
i-prince <molatonny@gmail.com>
2024-08-08 17:29:23 +0300

    Write the html code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,32 @@ import './App.css'
 function App() {
 
   return (
-    <>
-    
-    </>
+  <div className='body'>
+    <h1>
+      FAQs
+    </h1>
+
+    <h4>
+      What is Frontend Mentor and how will it help me?
+    </h4>
+
+    <p>
+      Frontend mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and Javascript. It's suitable for all levels and ideal for portfolio building.
+    </p>
+
+    <h4>
+      Is Frontend mentor free?
+    </h4>
+
+    <h4>
+      Can I use Frontend mentor projects in my portfolio?
+    </h4>
+
+    <h4>
+      How can i get help if I am stuck in a challenge?
+    </h4>
+
+  </div>
   )
 }
 
