a431649de459ad6778e0b529f0010cb55be87a1f
i-prince <molatonny@gmail.com>
2024-08-21 01:49:58 +0300

    Add more styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,38 @@ function App() {
   <div className='major'>
 
     <div className='mbody'>
-    <h1>
-      FAQs
-    </h1>
+    
+      <h1> 
+        <img className='img1' src='public/Project-Guidelines/assets/images/icon-star.svg'></img>
+        FAQs
+      </h1>
 
-    <h3>
-      What is Frontend Mentor and how will it help me?
-    </h3>
+      <h3 className='img2'>
+        What is Frontend Mentor and how will it help me?
+        <img src='public/Project-Guidelines/assets/images/icon-minus.svg'></img>
+      </h3>
 
-    <p>
-      Frontend mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and Javascript. It's suitable for all levels and ideal for portfolio building.
-    </p>
+      <p>
+        Frontend mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and Javascript. It's suitable for all levels and ideal for portfolio building.
+      </p>
 
-    <h3>
-      Is Frontend mentor free?
-    </h3>
+      <div className='dropdown'>
+        <h3>
+          Is Frontend mentor free?
+          <img src='public/Project-Guidelines/assets/images/icon-plus.svg'></img>
+        </h3>
 
-    <h3>
-      Can I use Frontend mentor projects in my portfolio?
-    </h3>
+        <h3>
+          Can I use Frontend mentor projects in my portfolio?
+          <img src='public/Project-Guidelines/assets/images/icon-plus.svg'></img>
+        </h3>
 
-    <h3>
-      How can i get help if I am stuck in a challenge?
-    </h3>
+        <h3>
+          How can i get help if I am stuck in a challenge?
+          <img src='public/Project-Guidelines/assets/images/icon-plus.svg'></img>
+        </h3>
+        
+      </div>
 
     </div>
     
